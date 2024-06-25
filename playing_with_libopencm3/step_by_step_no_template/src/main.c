872c5462dfa74c880b91c18324f35fef24f7991a
mohamed el gharbaoui <mohamed01gharbaoui@gmail.com>
2024-06-25 15:55:37 +0100

    free rtos SETUP

--- playing_with_libopencm3/step_by_step_no_template/src/main.c
+++ playing_with_libopencm3/step_by_step_no_template/src/main.c
@@ -1,22 +1,26 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+static void task1(void *args __attribute((unused))) {
+    for(;;) {
+        gpio_toggle(GPIOA, GPIO5);
+        __asm("nop");
+        vTaskDelay(pdMS_TO_TICKS(1500));
+    }
+}
 
 void system_setup(void);
 
 int main(void) {
-    system_setup();    
+    rcc_clock_setup_pll(&rcc_hse_16mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
 
-    volatile uint16_t read_value = 0;
-    while(1) {
-        read_value = gpio_get(GPIOC, GPIO13);
-        if (read_value & GPIO13) {
-            // means that there's no press
-            gpio_set(GPIOA, GPIO5);
-        } else {
-            // it means there was press
-            gpio_clear(GPIOA, GPIO5);
-        }
-    }
+    system_setup();
+    xTaskCreate(task1, "Task1", 1000, (void*)(configMAX_PRIORITIES-1), 1, NULL);
+
+    vTaskStartScheduler();
+
 
     while(1) {
         __asm("nop");
